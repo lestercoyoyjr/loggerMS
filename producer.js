eb4f33f39a83788ea5d74e7bf97721439f6d8d68
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-11-02 23:33:49 -0600

    ADD
    -> Create the exchange

--- producer.js
+++ producer.js
@@ -7,6 +7,16 @@ class Producer {
     channel;
 
     async createChannel () {
-        const connection = await amqp.connect()
+        const connection = await amqp.connect(config.rabbitMQ.url);
+        this.channel = await connection.createChannel();
+    }
+
+    async publishMessage(routingKey, message) {
+        if(!this.channel){
+            await this.createChannel();
+        }
+
+        const exchangeName = config.rabbitMQ.exchangeName;
+        await this.channel.assertExchange(exchangeName, "direct");
     }
 }
