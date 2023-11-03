0c86101f082af0dcb65f844f15dbf588613881bc
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-11-02 23:47:05 -0600

    ADD
    -> Publish the message to the exchange with a routing key

--- producer.js
+++ producer.js
@@ -1,22 +1,37 @@
-// we're gonna use it to produce the messages. 
+// we're gonna use it to produce the messages.
 
-const amqp = require('amqplib');
-const config = require('./config');
+const amqp = require("amqplib");
+const config = require("./config");
 
 class Producer {
-    channel;
+  channel;
 
-    async createChannel () {
-        const connection = await amqp.connect(config.rabbitMQ.url);
-        this.channel = await connection.createChannel();
+  async createChannel() {
+    const connection = await amqp.connect(config.rabbitMQ.url);
+    this.channel = await connection.createChannel();
+  }
+
+  async publishMessage(routingKey, message) {
+    if (!this.channel) {
+      await this.createChannel();
     }
 
-    async publishMessage(routingKey, message) {
-        if(!this.channel){
-            await this.createChannel();
-        }
+    const exchangeName = config.rabbitMQ.exchangeName;
+    await this.channel.assertExchange(exchangeName, "direct");
 
-        const exchangeName = config.rabbitMQ.exchangeName;
-        await this.channel.assertExchange(exchangeName, "direct");
-    }
+    const logDetails = {
+      logType: routingKey,
+      message: message,
+      dateTime: new Date(),
+    };
+    await this.channel.publish(
+      exchangeName,
+      routingKey,
+      Buffer.from(JSON.stringify(logDetails))
+    );
+
+    console.log(`The message ${message} is sent to exchange ${exchangeName}`)
+  }
 }
+
+module.exports = Producer;
