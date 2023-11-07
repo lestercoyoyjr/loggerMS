b3bc513790dad031c34d1b1383c2d2a942745969
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-11-07 00:38:01 -0600

    ADD
    Api method

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 // we're gonna use it for our API
 const express = require("express");
+const bodyParser = require("body-parser")
+const app = express();
+const Producer = require("./producer");
+const producer = new Producer();
+
+app.use(bodyParser.json("application/json"));
+
+// Publish the message
+app.post("/sendLog", async(req,res,next) => {
+
+})
